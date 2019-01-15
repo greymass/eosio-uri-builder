152505c6c35aeec13b00b612a4a1ed3f6b4f6319
Aaron Cox <aaron@greymass.com>
2019-01-14 23:25:44 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/uri-builder/precache-manifest.836371710cf92e3eea8090dff92caa47.js"
+  "/eosio-uri-builder/precache-manifest.43731225051e22bb4c19687143673a66.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/uri-builder/index.html", {
+workbox.routing.registerNavigationRoute("/eosio-uri-builder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
