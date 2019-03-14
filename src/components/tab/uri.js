d32936c8143084aec4de3fe01b4738847af4f168
Aaron Cox <aaron@greymass.com>
2019-03-14 12:46:21 -0400

    remove code highlight from URI

--- src/components/tab/uri.js
+++ src/components/tab/uri.js
@@ -37,19 +37,6 @@ class TabURI extends Component {
             </a>
           </Header.Subheader>
         </Header>
-        <Highlight {...defaultProps} code={exampleCode} language="jsx">
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-              {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
-            </pre>
-          )}
-        </Highlight>
         <Header size="small">
           URI Transaction:
         </Header>
